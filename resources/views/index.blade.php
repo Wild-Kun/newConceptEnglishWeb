f2690064aad9205ee58a246d738085d8255ac20b
王坤 <1941483751@qq.com>
2020-08-24 15:21:29 +0800

    页面布局

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('title', '首页')
+
+@section('content')
+<div class="row">
+    <div class="list">
+        <div class="list-item">
+            <div class="item-img">
+                <img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=501370926,1206112789&fm=26&gp=0.jpg"
+                    alt="">
+            </div>
+            <div>
+                <h4><a href="">标题</a></h4>
+            </div>
+        </div>
+    </div>
+
+</div>
+@stop
